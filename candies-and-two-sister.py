926a09986b4d0dac6b1b8f97e3aa323127166e2c
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-11-15 23:45:35 +0600

    candies-and-two-sister

---
+++ candies-and-two-sister.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range (t):
+    n = int(input())
+
+    if (n>2):
+        if (n%2 == 0):
+            print((n-2)//2)
+        else:
+            print((n-1)//2)
+    else:
+        print(0)
